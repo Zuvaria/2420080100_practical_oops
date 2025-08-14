6edd53cb2fc028b396aa2cb757a2cc92f9726cc8
Zuvaria <2420080100@klh.edu.in>
2025-08-14 09:34:26 +0530

    new file added to repository

---
+++ strings.java
@@ -0,0 +1,47 @@
+package myProject;
+
+public class strings {
+public static void main(String args[]) {
+	String str="hello this is java class";
+	String str1="hello";
+	String str2="Hello";
+	String str3="hi how are you";
+	String str4="hello";
+	String str5="1234";
+	String str6="Jva234";
+	String str7="";
+	String str8="   hello java    ";
+	System.out.println(str8.length());
+	/*System.out.println(str.toUpperCase());
+	System.out.println(str.toLowerCase());
+	System.out.println(str3.charAt(9));
+	System.out.println(str1.compareTo(str4));//returns integer 
+	System.out.println(str1.compareToIgnoreCase(str2));
+	System.out.println(str.concat(str2));
+	System.out.println(str1.contains("ell"));
+	System.out.println(str1.contentEquals(str4));
+	System.out.println(str1.endsWith("l"));*/
+	//System.out.println(str1.equals(str4));
+	//System.out.println(str1.indexOf("l"));
+	//System.out.println(str5.matches("[0-9]+"));
+	//System.out.println(str5.matches("\\d+"));
+	//System.out.println(str6.matches("[a-zA-Z0-9]+"));
+	//System.out.println(str7.isEmpty());
+	/*System.out.println(String.join("&",str2,str3,str4));
+	System.out.println(str1.lastIndexOf('l'));
+	System.out.println(str5.replace('l','p'));
+	System.out.println(str5.replaceFirst('hel','tor'));*/
+	/*String str10="hi;how;are;you";
+	String str9[]=str10.split(";");
+	for(String i:str9) 
+	System.out.println(i);
+	}*/
+		
+	/*System.out.println(str6.startsWith("J"));
+	System.out.println(str.subSequence(5,12));
+	System.out.println(String.valueOf(9.8f));*/
+	/*char arr[]=str4.toCharArray();
+	System.out.println(arr);*/
+	}
+
+}
